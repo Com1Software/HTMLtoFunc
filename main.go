76dc4aa92f368905604478f6eacaa82d7388b2cb
Lenovo 15 <information@com1software.com>
2023-06-11 11:55:39 -0400

    update

--- main.go
+++ main.go
@@ -9,19 +9,16 @@ import (
 func main() {
 	fmt.Printf("HTMLtoFunc Convert HTML Page to Go Function \n")
 	fmt.Printf("Operating System : %s\n", runtime.GOOS)
-
+	fmt.Printf("%d Parameters\n", len(os.Args))
 	switch {
 	//----------------------------
 	case len(os.Args) == 2:
-		cmd := os.Args[1]
-		switch {
-		case cmd == "overview":
-			fmt.Printf("Overview\n")
-
-		default:
-
-		}
+		file := os.Args[1]
+		fmt.Printf("File : %s\n", file)
 	case len(os.Args) == 1:
+		file := "data/test.html"
+		fmt.Printf("File : %s\n", file)
+
 	}
 
 }
